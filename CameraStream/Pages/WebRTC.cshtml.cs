859cc0934f1502d97c7e2389d08b96315e904ae2
Yaseer Arafat <yaseer,arafat@live.com>
2022-04-02 18:55:25 +0600

    first commit

---
+++ CameraStream/Pages/WebRTC.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CameraStream.Pages
+{
+    public class WebRTCModel : PageModel
+    {
+        private readonly ILogger<WebRTCModel> _logger;
+
+        public WebRTCModel(ILogger<WebRTCModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
